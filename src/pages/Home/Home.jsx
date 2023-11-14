980da6b0e52e116bfb4604048e4d29f9f19584de
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-11-14 11:13:14 +0300

    2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,19 +3,19 @@ import { getTrendingMovies } from '../../components/services/getMovies';
 import FilmsList from '../../components/FilmList/FilmList';
 import { PageButtons } from "../../components/Buttons/PageButtons"
 import { Loader } from '../../components/Loader/Loader';
-
+// import { useSearchParams } from 'react-router-dom';
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [data, setData] = useState({});
    const [page, setPage] = useState(1);
    const [isLoading, setIsLoading] = useState(false);
  const [error, setError] = useState(null);
+//  const [searchParams, setSearchParams] = useSearchParams();
 
-  
-   
   useEffect(() => {
-  
+    
     const fetchMovie = async () => {
+     
       try {
         setIsLoading(true);
         const data = await getTrendingMovies(page);
