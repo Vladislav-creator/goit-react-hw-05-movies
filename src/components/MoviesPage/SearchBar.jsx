e68b9d40ca03a289936fe41c9f1c51b94a9de2db
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-11-15 00:08:28 +0300

    2

--- src/components/MoviesPage/SearchBar.jsx
+++ src/components/MoviesPage/SearchBar.jsx
@@ -1,13 +1,48 @@
+// import { useState } from 'react';
+// import css from './MoviesPage.module.css';
+
+// const Form = ({ setSearchParams }) => {
+//   const [query, setQuery] = useState('');
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+
+//     setSearchParams({ query });
+//   };
+
+//   const handleSearchParams = ({ target: { value } }) => {
+//     setQuery(value);
+//   };
+
+//   return (
+//     <form className={css.searchForm} onSubmit={handleSubmit}>
+//       <input className={css.searchInput}
+//         type="text"
+//         placeholder="Name movie"
+//         autoFocus
+//         value={query}
+//         onChange={handleSearchParams}
+//       />
+//       <button className={css.searchButton} type="submit" disabled={!query}>
+//         Search
+//       </button>
+//     </form>
+//   );
+// };
+// export default Form;
+
 import { useState } from 'react';
+
 import css from './MoviesPage.module.css';
 
-const Form = ({ setSearchParams }) => {
+const Form = ({onSubmit}) => {
   const [query, setQuery] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
-
-    setSearchParams({ query });
+    if(!query)return
+    onSubmit(query)
+  
   };
 
   const handleSearchParams = ({ target: { value } }) => {
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,4 +1,68 @@
-import { useEffect, useState, useCallback } from 'react';
+// import { useEffect, useState, useCallback } from 'react';
+// import { useSearchParams } from 'react-router-dom';
+// import { getMovieByQuery } from '../../components/services/getMovies';
+// import { PageButtons } from 'components/Buttons/PageButtons';
+// import Form from '../../components/MoviesPage/SearchBar';
+// import FilmsList from '../../components/FilmList/FilmList';
+// import { Loader } from '../../components/Loader/Loader';
+// const Movies = () => {
+//   const [data, setData] = useState({});
+//   const [page, setPage] = useState(1);
+//   const [searchParams, setSearchParams] = useSearchParams();
+
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const currentQuery = searchParams.get('query');
+//      if (!currentQuery) return;
+//    console.log(currentQuery);
+
+//     const fetchMovieByQuery = async () => {
+//       const currentPage = Number(searchParams.get('page'))||1; 
+//       try {
+//         setIsLoading(true);
+//         const data = await getMovieByQuery(currentQuery, page);
+        
+//          setData(data);
+//           setPage(currentPage);
+        
+//       } catch (error) {
+//           setError(error.message);
+//         } finally {
+//           setIsLoading(false);
+//         }
+//     };
+//     fetchMovieByQuery();
+//   }, [searchParams, page]);
+
+//   const handlePageChange = useCallback(
+//     page => {
+//       setPage(page);
+//       setSearchParams({ page });
+//     },
+//     [ setSearchParams, setPage]
+//   );
+
+//   return (
+//     <div className="container">
+//       <Form setSearchParams={setSearchParams} />
+//       {error !== null && <p className="error-bage">{error}</p>}
+//       {isLoading && <Loader />}
+//       {data.total_pages > 0 && <FilmsList movies={data.results} />}
+//       {data.total_pages > 1 && (
+//               <PageButtons
+//                 page={page}
+//                 totalPages={data.total_pages}
+//                 handlePageChange={handlePageChange}
+//               />
+//              )} 
+//     </div>
+//   );
+// };
+// export default Movies;
+
+import { useEffect, useState} from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { getMovieByQuery } from '../../components/services/getMovies';
 import { PageButtons } from 'components/Buttons/PageButtons';
@@ -7,28 +71,23 @@ import FilmsList from '../../components/FilmList/FilmList';
 import { Loader } from '../../components/Loader/Loader';
 const Movies = () => {
   const [data, setData] = useState({});
-  const [page, setPage] = useState(1);
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const [page, setPage] = useState(searchParams.get('page')||1);
+  const [query, setQuery] = useState(searchParams.get('query')||"");
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const currentQuery = searchParams.get('query');
-     if (!currentQuery) return;
-   console.log(currentQuery);
+     if (!query) return;
+     setSearchParams({page: page.toString(),query:query.toString()});
 
     const fetchMovieByQuery = async () => {
-      const currentPage = Number(searchParams.get('page'))||1; 
       try {
         setIsLoading(true);
-        const data = await getMovieByQuery(currentQuery, page);
-        //  console.log(page);
-        // console.log(currentPage);
+        const data = await getMovieByQuery(query, page);
          setData(data);
-          setPage(currentPage);
-        
-
       } catch (error) {
           setError(error.message);
         } finally {
@@ -36,19 +95,20 @@ const Movies = () => {
         }
     };
     fetchMovieByQuery();
-  }, [searchParams, page]);
+  }, [setSearchParams, page,query]);
 
-  const handlePageChange = useCallback(
+  const handlePageChange =
     page => {
       setPage(page);
-      setSearchParams({ page });
-    },
-    [ setSearchParams, setPage]
-  );
+    }
 
+  const onSubmit=(query)=>{
+    setQuery(query)
+    setPage(1)
+  }
   return (
     <div className="container">
-      <Form setSearchParams={setSearchParams} />
+      <Form onSubmit={onSubmit}/>
       {error !== null && <p className="error-bage">{error}</p>}
       {isLoading && <Loader />}
       {data.total_pages > 0 && <FilmsList movies={data.results} />}
@@ -58,7 +118,7 @@ const Movies = () => {
                 totalPages={data.total_pages}
                 handlePageChange={handlePageChange}
               />
-             )} 
+             )}
     </div>
   );
 };
